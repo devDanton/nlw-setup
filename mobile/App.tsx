dac6f28c893eb62d4f6ce7d763f891b5c2079eb5
Danton da Rosa Abreu <77130559+devDanton@users.noreply.github.com>
2023-01-20 13:35:38 -0300

    Add icon, splash, favicon

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold
+} from '@expo-google-fonts/inter';
+import { Loading } from './src/components/Loadfing';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <Loading />
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Olá mundo</Text>
-      <StatusBar style='auto' />
+      <Text style={styles.text}>Teste</Text>
+      <StatusBar barStyle={'light-content'} backgroundColor="transparent" translucent />
     </View>
   );
 }
@@ -22,5 +42,6 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#ffffff',
+    fontFamily: 'Inter_800ExtraBold'
   }
 });
