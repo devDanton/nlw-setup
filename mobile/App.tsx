31ec8fc586ff25d332a26b28d425d78449d4ffac
Danton da Rosa Abreu <77130559+devDanton@users.noreply.github.com>
2023-01-30 16:03:28 -0300

    Home mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,5 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import './src/lib/dayjs'
+import { StatusBar } from 'react-native';
 import {
   useFonts,
   Inter_400Regular,
@@ -7,6 +8,7 @@ import {
   Inter_800ExtraBold
 } from '@expo-google-fonts/inter';
 import { Loading } from './src/components/Loadfing';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -23,25 +25,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Teste</Text>
+    <>
+      <Home />
       <StatusBar barStyle={'light-content'} backgroundColor="transparent" translucent />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderColor: '#ffffff',
-    borderStyle: 'solid',
-    borderWidth: 1,
-  },
-  text: {
-    color: '#ffffff',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
